f15b6aa451ac76928c1944308f87769f7b8a091e
Francisco Chagas <kikochagas1987@gmail.com>
2023-05-17 19:31:03 +0100

    finished section 5

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from "uuid";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+  public characters: Character[] =
+  [{
+    id: uuid(),
+    name: 'Krillin',
+    power: 500
+  },
+  {
+    id: uuid(),
+    name: 'Goku',
+    power: 9500
+  },
+  {
+    id: uuid(),
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  onNew(character: Character): void {
+      const newCharacter: Character = { ...character, id: uuid()  }
+
+      this.characters.push(newCharacter);
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.characters = this.characters.filter(x=>x.id !== id)
+  }
+
+}
